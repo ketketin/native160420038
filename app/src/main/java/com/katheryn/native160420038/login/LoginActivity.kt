082d41c4607ffc3badd530b2a799a27a794069ce
ketketin <114962909+ketketin@users.noreply.github.com>
2023-03-01 14:00:41 +0700

    Tugas Week 1 ANMP: Membuat login

---
+++ app/src/main/java/com/katheryn/native160420038/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.katheryn.native160420038.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.katheryn.native160420038.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
