acc7c8f8c627130bef6dc4d534e624b7f9b5b918
marianajunita17 <ymawuntu14@gmail.com>
2023-03-01 14:35:04 +0700

    Tugas Week 1 ANMP: update login

--- app/src/main/java/com/katheryn/native160420038/login/LoginActivity.kt
+++ app/src/main/java/com/katheryn/native160420038/login/LoginActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import com.katheryn.native160420038.R
 
 class LoginActivity : AppCompatActivity() {
+    var user = 0
+    var pass = 12345678
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
 }
